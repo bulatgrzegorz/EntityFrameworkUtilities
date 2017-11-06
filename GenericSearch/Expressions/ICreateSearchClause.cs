883b52718c93f2a1ce49f6a2426b6041364962d5
lotrzyk <grzegorz0bulat@gmail.com>
2017-11-06 22:57:43 +0100

    ICreateSearchClause interface, enum for strategy

---
+++ GenericSearch/Expressions/ICreateSearchClause.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace GenericSearch.Expressions
+{
+    public interface ICreateSearchClause
+    {
+        Expression<Func<T, bool>> CreateSearchClause<T>(ISearchableEntity entity);
+    }
+}
---
+++ GenericSearch/Expressions/SearchClauseStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericSearch.Expressions
+{
+    public enum SearchClauseStrategy
+    {
+        Normal = 0,
+        ByContains = 1,
+        ByRange = 2,
+        CombiningByOr = 3
+    }
+}
