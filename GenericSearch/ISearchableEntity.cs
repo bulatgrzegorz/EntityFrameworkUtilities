75a93bdfbe2235078dfc7f6b785a08734ffe67ec
lotrzyk <grzegorz0bulat@gmail.com>
2017-11-06 21:56:00 +0100

    SearchableEntity interface that is base for creating generic expresions search

---
+++ GenericSearch/ISearchableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericSearch
+{
+    public interface ISearchableEntity
+    {
+        string ColumnNameToSearchBy { get; set; }
+        
+        object ValueToSearch { get; set; }
+        
+        object AdditionalValue { get; set; }
+        
+        bool IsSearchByContains { get; set; }
+        
+        Type ValueType { get; set; } 
+    }
+}
