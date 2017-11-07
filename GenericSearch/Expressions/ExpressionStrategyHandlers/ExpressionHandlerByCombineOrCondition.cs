c657a52ff3dbf366313efb03f20d515f1548c5bb
lotrzyk <grzegorz0bulat@gmail.com>
2017-11-07 22:45:38 +0100

    Start of expression handler for combining or conditions

---
+++ GenericSearch/Expressions/ExpressionStrategyHandlers/ExpressionHandlerByCombineOrCondition.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace GenericSearch.Expressions.ExpressionStrategyHandlers
+{
+    public class ExpressionHandlerByCombineOrCondition : IPrepareExpressionBaseOnStrategy
+    {
+        public SearchClauseStrategy SearchStrategy { get; set; }
+
+        public Expression<Func<T, bool>> CreateExpression<T>(
+            ISearchableEntity entity, 
+            ParameterExpression parameterExpression,
+            MemberExpression memberExpression)
+        {
+            if (!(entity.ValueToSearch is IList))
+            {
+                throw new ArgumentException("Type of value to search by is wrong.");
+            }
+
+            var valueConvertedToList = (IList) entity.ValueToSearch;
+
+            if (valueConvertedToList.Count == 0) return null;
+            if (valueConvertedToList.Count == 1)
+            {
+                var normalExpressionHandler = new ExpressionHandlerNormal();
+                return normalExpressionHandler.CreateExpression<T>(entity, parameterExpression, memberExpression);
+            }
+
+            return null;
+        }
+
+        public ExpressionHandlerByCombineOrCondition()
+        {
+            SearchStrategy = SearchClauseStrategy.CombiningByOr;
+        }
+    }
+}
---
+++ GenericSearchTests/Expressions/ExpressionStrategyHandlers/ExpressionHandlerByCombineOrConditionTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using GenericSearch;
+using GenericSearch.Expressions;
+using GenericSearch.Expressions.ExpressionStrategyHandlers;
+using NUnit.Framework;
+
+namespace GenericSearchTests.Expressions.ExpressionStrategyHandlers
+{
+    [TestFixture]
+    public class ExpressionHandlerByCombineOrConditionTests
+    {
+        private ExpressionHandlerByCombineOrCondition _expressionHandler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _expressionHandler = new ExpressionHandlerByCombineOrCondition();
+        }
+        
+        [Test]
+        [TestCase(default (int))]
+        [TestCase(default (string))]
+        [TestCase(default (bool))]
+        [TestCase(default (IDictionary<string, object>))]
+        public void CreateExpression_ThrowsArgumentExceptionWhenTypeIsNotOfTypeCollectionString(object passedValue)
+        {
+            var testEntity = new TestClass()
+            {
+                ValueToSearch = passedValue
+            };
+
+            Assert.Throws<ArgumentException>(() => _expressionHandler.CreateExpression<int>(testEntity, null, null));
+        }
+
+        [Test]
+        public void CreateExpression_DoesNotThrowException_WhenPassedValueIsListOfString()
+        {
+            Assert.DoesNotThrow(() => _expressionHandler.CreateExpression<int>(
+                CreateTestClass(new List<string> {"123", "1111"}), 
+                null, 
+                null));
+        }
+        
+        [Test]
+        public void CreateExpression_DoesNotThrowException_WhenPassedValueArrayOfString()
+        {
+            Assert.DoesNotThrow(() => _expressionHandler.CreateExpression<int>(
+                CreateTestClass(new[] {"123", "1111"}), 
+                null, 
+                null));
+        }
+
+        private static TestClass CreateTestClass(object valueOfEntity)
+        {
+            return  new TestClass()
+            {
+                ValueToSearch = valueOfEntity
+            };
+        }
+        
+        private class TestClass : ISearchableEntity
+        {
+            public string ColumnNameToSearchBy { get; set; }
+            public object ValueToSearch { get; set; }
+            public object AdditionalValue { get; set; }
+            public SearchClauseStrategy SearchStrategy { get; set; }
+            public Type ValueType { get; set; }
+        }
+    }
+}
